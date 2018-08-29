b9901679877bb82599353c93e55394d9e4a61e85
w530385371 <530385371@qq.com>
2018-08-29 15:42:36 +0800

    打包组件npm run build，发布到npm publish

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-import ButtonGroup from './src/button-group'
-import Button from './src/button'
-import Icon from './src/icon'
-
+import './src/assets/css/index'
+import ButtonGroup from './src/components/button-group'
+import Button from './src/components/button'
+import Icon from './src/components/icon'
+Vue.component('iue-button-group',ButtonGroup)
+Vue.component('iue-button',Button)
+Vue.component('iue-icon',Icon)
